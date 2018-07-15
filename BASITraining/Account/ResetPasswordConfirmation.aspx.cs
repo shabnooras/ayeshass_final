45b4c50df88e504b609a01526ca957a6eb8535bb
shabnooras <40215499+shabnooras@users.noreply.github.com>
2018-07-15 21:32:31 +0100

    Add project files.

---
+++ BASITraining/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BASITraining.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BASITraining/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BASITraining.Global" Language="C#" %>
